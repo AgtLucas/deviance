38b7c9f10f833c8c6ebb34f2710998dec1f68505
Lucas Churchill <lucas@agtlucas.com>
2014-06-17 18:16:27 -0300

    Update Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    meta: {
+      pathJS: 'public/assets/js',
+      pathJSBuild: 'public/build/js'
+    },
+
+    browserify: {
+      dist: {
+        files: {
+          '<%= meta.pathJSBuild %>/app.js': ['<%= meta.pathJS %>/**/*.js']
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['browserify']);
+
+};
